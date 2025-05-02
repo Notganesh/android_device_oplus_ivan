475be34933641e35fcd424008ff9b4929de4e029
Notganesh <chimeports@gmail.com>
2025-05-02 10:32:17 +0530

    ivan:prebuilt dtbo from stock

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,9 +22,16 @@ include device/oplus/mt6877-common/BoardConfigCommon.mk
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS := 0x2000U
 
+# Assertation
+TARGET_OTA_ASSERT_DEVICE := ivan,IV2201,IV2201EEA,OP555BL1
+
 # Init
 TARGET_INIT_VENDOR_LIB ?= //$(DEVICE_PATH):init_ivan
 TARGET_RECOVERY_DEVICE_MODULES ?= init_ivan
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineage-ivan_defconfig
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 
 # Call proprietary blob setup
 include vendor/oplus/ivan/BoardConfigVendor.mk
